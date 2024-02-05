ca3e043c9c42a442bbbfb4c64534afdabcaf434d
Bryan Crossman <cros0400@umn.edu>
2024-02-05 10:35:47 -0600

    Adding order table, modify var names to reflect usage

--- label-gui/sql/labeling_schema.sql
+++ label-gui/sql/labeling_schema.sql
@@ -1,15 +1,28 @@
-create table Board
+create table Label
 (
-	board_id int unsigned auto_increment,
-	full_sn varchar(15) not null,
+	label_id int unsigned auto_increment,
+	full_label varchar(15) not null,
 	type_sn int(6) not null,
 	type_code varchar(6) not null,
 	sn int(6) not null,
 	major_type_id int unsigned,
 	sub_type_id int unsigned, 
+	creation_date datetime,
+	order_id int not null,
 	primary key (board_id),
 	unique(full_sn)
 );
+
+create table Order_Info
+(
+	order_id int unsigned auto_increment, 
+	name varchar(50),
+	email varchar(50),
+	location varchar(50),
+	order_date datetime,
+	fulfillment_date datetime,
+	primary key (order_id)
+)
 
 create table Major_Type
 (
@@ -24,6 +37,7 @@ create table Sub_Type
 (
 	sub_type_id int unsigned auto_increment,
 	primary key (sub_type_id),
+	digits int(2) unsigned not null,
 	sub_sn int(4) unsigned not null,
 	sub_code varchar(4) not null,
 	name varchar(50) not null,
